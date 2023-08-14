dbc6fe549cf7ccefa8b00c1570829893bf7013b4
AlexanderInUM <113099460+AlexanderInUM@users.noreply.github.com>
2023-08-14 10:01:39 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import socket
+
+def start_udp_server():
+    HOST = "127.0.0.1"
+    PORT = 9393
+    BUFFER  = 1024 
+    # Create a datagram socket
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # Bind to address and ip
+    UDPServerSocket.bind((HOST, PORT))
+    print("UDP server up and listening")
+    # Listen for incoming datagrams
+    while(True):
+        bytesAddressPair = UDPServerSocket.recvfrom(BUFFER)
+        message = bytesAddressPair[0]
+        address = bytesAddressPair[1]
+        clientMsg = message.decode()
+        _clientMsg = "Message from Client: {}".format(clientMsg)
+        _clientIP  = "Client IP Address: {}".format(address)
+        print(_clientMsg)
+        print(_clientIP)
+        # Sending a reply to client
+        msgFromServer       = "UDP client connection state running on IP:" + address
+        bytesToSend         = str.encode(msgFromServer)
+        UDPServerSocket.sendto(bytesToSend, address)
+
+
+start_udp_server()
